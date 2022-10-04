3b848df17102fcdaf45a266d68c31871c09bd4b3
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-04 08:24:04 -0300

    aplico logger

--- src/routes/infoRouter.js
+++ src/routes/infoRouter.js
@@ -5,17 +5,21 @@ import logger from '../utils/logger.js';
 const infoRouter = new Router();
 
 infoRouter.get('/info', logger.logReqInfo, (req, res) => {
-    const info = {
-        os: process.platform,
-        nodeVersion: process.version,
-        rss: process.memoryUsage().rss,
-        directory: process.cwd(),
-        pid: process.pid,
-        path: process.execPath,
-        args: process.argv,
-        cpus: os.cpus().length
-    };
-    res.render('info', info);
+    try {
+        const info = {
+            os: process.platform,
+            nodeVersion: process.version,
+            rss: process.memoryUsage().rss,
+            directory: process.cwd(),
+            pid: process.pid,
+            path: process.execPath,
+            args: process.argv,
+            cpus: os.cpus().length
+        };
+        res.render('info', info);
+    } catch (error) {
+        logger.logError(error);
+    }
 })
 
 export default infoRouter;
