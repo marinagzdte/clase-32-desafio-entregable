7177e97a4e6e17923ff54b76d990e183dadcccdd
Marina Gonzalez Dante <mgonzalezdante@hotmail.com>
2022-10-04 08:23:06 -0300

    add config log4js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import * as dotenv from 'dotenv';
-const res = dotenv.config({ path: './.env' });
+dotenv.config({ path: './.env' });
 
 export default {
     mariaDb: {
@@ -16,6 +16,23 @@ export default {
         options: {
             useNewUrlParser: true,
             useUnifiedTopology: true
+        }
+    },
+    log4js: {
+        appenders: {
+            info: { type: "console" },
+            errFile: { type: "file", filename: "error.log" },
+            warnFile: { type: "file", filename: "warn.log" },
+
+            log: { type: 'logLevelFilter', appender: 'info', level: 'all' },
+            errLog: { type: 'logLevelFilter', appender: 'errFile', level: 'error' },
+            warnLog: { type: 'logLevelFilter', appender: 'warnFile', level: 'warn' },
+        },
+        categories: {
+            default: {
+                appenders: ['log', 'errLog', 'warnLog'],
+                level: 'all'
+            }
         }
     }
 }
